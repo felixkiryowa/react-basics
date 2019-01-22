6ddd8805ecaabf2d2633c702f81d894fbe878a33
Francis Kiryowa <franciskiryowa68@gmail.com>
2019-01-21 22:09:07 -0500

    Add react components and functions

---
+++ src/Components/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  render() {
+    return (
+      <ul className="List">
+         <li>1</li>
+         <li>2</li>
+         <li>3</li>
+      </ul>
+    );
+  }
+}
+
+export default List;
---
+++ src/Components/MyInfo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Footer from './Footer';
+
+function  MyInfo(){
+    return (
+        <div className="MyInfo">
+           <nav>
+            <h1>Francis Kiryowa</h1>
+                <ul className="vacations">
+                    <li>Kasese</li>
+                    <li>Kyotera</li>
+                    <li>Mumbasa</li>
+                </ul>
+           </nav>
+           <main>
+
+           </main>
+           <Footer />
+          
+        </div>
+    );
+  }
+
+export default MyInfo;
